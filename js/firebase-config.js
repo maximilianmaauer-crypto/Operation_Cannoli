d98f7c5dddd8e3f0d382e2115846448ff74bfa39
maximilianmaauer-crypto <maximilian.ma.auer@googlemail.com>
2026-06-01 14:09:44 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,10 +2,10 @@
 // Damit die Einkaufsliste für alle sichtbar ist, muss hier die Firebase-Web-App-Konfiguration eingefügt werden.
 // Firebase Console → Project settings → General → Your apps → Web app → SDK setup and configuration → Config
 export const firebaseConfig = {
-    apiKey: "AIzaSyCQaD2kpwEYKVDreCYmcMy9kz_fP5XuudA",
-    authDomain: "operation-cannoli-e37d1.firebaseapp.com",
-    projectId: "operation-cannoli-e37d1",
-    storageBucket: "operation-cannoli-e37d1.firebasestorage.app",
-    messagingSenderId: "795404337053",
-    appId: "1:795404337053:web:b1b3f9dfcdd1179cb7b974"
-  };
+  apiKey: "AIzaSyCQaD2kpwEYKVDreCYmcMy9kz_fP5XuudA",
+  authDomain: "operation-cannoli-e37d1.firebaseapp.com",
+  projectId: "operation-cannoli-e37d1",
+  storageBucket: "operation-cannoli-e37d1.firebasestorage.app",
+  messagingSenderId: "795404337053",
+  appId: "1:795404337053:web:b1b3f9dfcdd1179cb7b974"
+};
